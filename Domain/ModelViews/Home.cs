d153f3409678e8fc1170987733f279e8039b1a7e
Luis Starlino <luis.guilherme009@gmail.com>
2025-02-10 19:29:27 -0300

    feat(config): finish the config

---
+++ Domain/ModelViews/Home.cs
@@ -0,0 +1,8 @@
+﻿namespace _NET_MinimalAPI.Domain.ModelViews
+{
+    public struct Home
+    {
+        public string Message { get => "Welcome to Minimal API Vehicles"; }
+        public string Doc { get => "/swagger"; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using _NET_MinimalAPI.Domain.Interfaces;
+using _NET_MinimalAPI.Domain.ModelViews;
 using _NET_MinimalAPI.Domain.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -6,6 +7,9 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdministratorService, AdministratorService>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DBContext>(options => {
 
@@ -16,9 +20,12 @@ builder.Services.AddDbContext<DBContext>(options => {
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+#region Home
+app.MapGet("/", () => Results.Json(new Home()));
+#endregion
 
-app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministratorService administratorService) =>
+#region Admin
+app.MapPost("admin/login", ([FromBody] LoginDTO loginDTO, IAdministratorService administratorService) =>
 {
 
     if (administratorService.Login(loginDTO) != null)
@@ -31,6 +38,10 @@ app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministratorService admin
     }
 
 });
+#endregion
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
 
